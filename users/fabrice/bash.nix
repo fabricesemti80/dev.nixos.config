7e78a0e4d0cccd8ffe52774d9d315e21d9d3c181
Fabrice Semti <fabrice@fabricesemti.com>
2023-11-18 18:26:15 +0000

    feat: 🚀 load `direnv` with bash

--- users/fabrice/bash.nix
+++ users/fabrice/bash.nix
@@ -5,8 +5,9 @@ in
   programs.bash = {
     enable = true;
     initExtra = ''
-      eval "$(starship init bash)"
-      alias cat='bat'
+      eval "$(starship init bash)" # enable `starship`
+      eval "$(direnv hook bash)"   # enable `direnv`
+      alias cat='bat'              # cat is now bat!
     '';
   };
 }
