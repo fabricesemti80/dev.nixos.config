37907845a42b3666fdc429f602180ab137d002b8
Fabrice Semti <fabrice@fabricesemti.com>
2023-11-18 16:30:30 +0000

    feat: 🚀 review Git alieases
    - https://chat.openai.com/c/f0644ef8-7345-4e05-a401-1059bef11b01
    - https://gist.github.com/mwhite/6887990

--- users/fabrice/git.nix
+++ users/fabrice/git.nix
@@ -1,18 +1,31 @@
 { lib, ... }:
+
 let
+  gitAliases = {
+    l = "log --pretty=format:\"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [ %cn ]\" --decorate --date=short";
+    a = "add";
+    ap = "add -p";
+    c = "commit --verbose";
+    ca = "commit -a --verbose";
+    cm = "commit -m";
+    cam = "commit -a -m";
+    m = "commit --amend --verbose";
+    d = "diff";
+    ds = "diff --stat";
+    dc = "diff --cached";
+    s = "status -s";
+    co = "checkout";
+    cob = "checkout -b";
+    b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+    la = "!git config -l | grep alias | cut -c 7-";
+  };
+
 in
 {
   programs.git = {
     enable = true;
     userName = "Fabrice Semti";
     userEmail = "fabrice@fabricesemti.com";
-    aliases = {
-      gcam = "git commit -a -m";
-      gco = "git checkout";
-      gu = "git push -u origin HEAD";
-      gd = "git pull origin";
-      gh = "git checkout master";
-      gb = "git branch";
-    };
+    aliases = gitAliases;
   };
 }
